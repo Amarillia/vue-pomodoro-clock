d11c6c1b11b462883ee18d940183db43e80bb8a2
Edina Orban <Amarillia@users.noreply.github.com>
2019-08-17 22:04:31 +0100

    basic layout

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <AppButton name="Break Decrement" id="break-decrement" />
+    <AppButton name="Session Decrement" id="session-decrement" />
+    <AppButton name="Break Increment" id="break-increment" />
+    <AppButton name="Session Increment" id="session-increment" />
+    <p name="Break Length" id="break-label"></p>
+    <p name="Session Length" id="session-label"></p>
+    <p name="Session" id="timer-label"></p>
+    <p name="Time Left" id="time-left">
+      <span id="timer-mins" class="timer">25:</span>
+      <span id="timer-secs" class="timer">00</span>
+    </p>
+  </div>
+</template>
+
+<script>
+import AppButton from "./components/AppButton.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppButton
+  }
+};
+</script>
+
+<style>
+/* Colours */
+/* #A66D78
+#BF9B6F
+#BF8069
+#D96B62 */
+
+* {
+  background-color: #590209;
+  color: #fff;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.timer {
+  font-size: 100px;
+}
+</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <button class="myButton">
+      <slot>{{name}}</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    name: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.myButton {
+  -moz-box-shadow: 0px 10px 14px -7px #8a240b;
+  -webkit-box-shadow: 0px 10px 14px -7px #8a240b;
+  box-shadow: 0px 10px 14px -7px #8a240b;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #db6e53),
+    color-stop(1, #9d4f3c)
+  );
+  background: -moz-linear-gradient(top, #db6e53 5%, #9d4f3c 100%);
+  background: -webkit-linear-gradient(top, #db6e53 5%, #9d4f3c 100%);
+  background: -o-linear-gradient(top, #db6e53 5%, #9d4f3c 100%);
+  background: -ms-linear-gradient(top, #db6e53 5%, #9d4f3c 100%);
+  background: linear-gradient(to bottom, #db6e53 5%, #9d4f3c 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#db6e53', endColorstr='#9d4f3c',GradientType=0);
+  background-color: #db6e53;
+  -moz-border-radius: 8px;
+  -webkit-border-radius: 8px;
+  border-radius: 8px;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 13px 32px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #d53811;
+  border: none;
+  margin: 10px;
+}
+.myButton:hover {
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #9d4f3c),
+    color-stop(1, #db6e53)
+  );
+  background: -moz-linear-gradient(top, #9d4f3c 5%, #db6e53 100%);
+  background: -webkit-linear-gradient(top, #9d4f3c 5%, #db6e53 100%);
+  background: -o-linear-gradient(top, #9d4f3c 5%, #db6e53 100%);
+  background: -ms-linear-gradient(top, #9d4f3c 5%, #db6e53 100%);
+  background: linear-gradient(to bottom, #9d4f3c 5%, #db6e53 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#9d4f3c', endColorstr='#db6e53',GradientType=0);
+  background-color: #9d4f3c;
+}
+.myButton:active {
+  position: relative;
+  top: 1px;
+}
+
+.container {
+  text-align: left;
+  max-width: 40%;
+}
+</style>
